c2e8089d1df408743e9bcd99f85e8feaf861973c
Oleg <olegashepelev@gmail.com>
2023-01-13 00:09:46 +0600

    Complete second task

---
+++ 02/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int num = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+int sumNumbers = GetSumNumbers(num);
+Console.WriteLine($"{num} -> {sumNumbers}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetSumNumbers(int number)
+{
+    int sum = 1;
+    while (number > 0)
+    {
+        sum = sum * number;
+        number--;
+    }
+    return sum;
+}
